04a3c62810261a3a80c77a6daedd8e30d85a5757
Petr Sládek <petr.sladek@i.cz>
2017-10-09 12:39:01 +0200

    Creating the Java Int Problem example file.

---
+++ src/ps/java/intproblem/JavaIntProblem.java
@@ -0,0 +1,10 @@
+package ps.java.intproblem;
+
+public class JavaIntProblem {
+
+    public static void main(String[] args) {
+        int x = 33333333;
+        x += 1.0f;
+        System.out.println(x);
+    }
+}
